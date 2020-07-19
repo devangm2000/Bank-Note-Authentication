2680d837d6b8c0a7d8c96c2aa734375768f93d8b
Devang Mehrotra <49409432+devangm2000@users.noreply.github.com>
2020-07-19 22:36:39 +0530

    Add files via upload

--- ModelTraining.py
+++ ModelTraining.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[3]:
+# In[2]:
 
 
 df=pd.read_csv("BankNote_Authentication.csv")
 
 
-# In[4]:
+# In[3]:
 
 
 df.head()
 
 
-# In[22]:
+# In[4]:
 
 
-X=df.iloc[:,:-1]
-y=df.iloc[:,-1]
+X=df.iloc[:,:-1].values
+y=df.iloc[:,-1].values
 
 
-# In[25]:
+# In[5]:
 
 
-X.head(10)
+X
 
 
-# In[28]:
+# In[6]:
 
 
-y.head(10)
+y
 
 
-# In[29]:
+# In[7]:
 
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25,random_state=42)
 
 
-# In[30]:
+# In[8]:
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -54,38 +54,57 @@ classifier=RandomForestClassifier()
 classifier.fit(X_train,y_train)
 
 
-# In[31]:
+# In[9]:
 
 
 y_pred=classifier.predict(X_test)
 
 
-# In[38]:
+# In[10]:
 
 
 #Accuracy using confusion matrix 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-print(cm)
+cm
 
 
-# In[39]:
-
-
-accuracy=(cm[0][0]+cm[1][1])/(cm[0][0]+cm[0][1]+cm[1][0]+cm[1][1])
-print(accuracy)
-
-
-# In[42]:
+# In[11]:
 
 
 #Accuracy
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(y_test, y_pred)
-print(acc)
+acc
 
 
-# In[43]:
+# In[12]:
+
+
+tp=cm[0][0]
+fp=cm[0][1]
+tn=cm[1][1]
+fn=cm[1][0]
+precision=tp/(tp+fp)
+precision
+
+
+# In[13]:
+
+
+recall=tp/(tp+fn)
+recall
+
+
+# In[14]:
+
+
+#F1 = 2 * (precision * recall) / (precision + recall)
+from sklearn.metrics import f1_score
+f1_score(y_test, y_pred, zero_division=1)
+
+
+# In[15]:
 
 
 #Pickle file
@@ -95,11 +114,11 @@ pickle.dump(classifier,pickle_out)
 pickle_out.close()
 
 
-# In[45]:
+# In[16]:
 
 
 #prediction
-print(classifier.predict([[2,3,4,1]]))
+classifier.predict([[2,3,4,1]])
 
 
 # In[ ]:
